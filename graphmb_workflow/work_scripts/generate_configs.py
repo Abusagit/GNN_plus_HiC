0a5f199ba4364c00dae984a91017dcc29a320351
Abusagit <mirotvorez00@gmail.com>
2022-10-04 18:53:44 +0000

    feat add template for config construction

--- graphmb_workflow/work_scripts/generate_configs.py
+++ graphmb_workflow/work_scripts/generate_configs.py
@@ -1,15 +1,29 @@
 import argparse
 from pathlib import Path
+from tqdm import tqdm
+import pandas as pd
+
+from GNN_plus_HiC.graphmb_workflow.construct_graphmb_wf_config import create_json
 
 def get_parser():
     parser = argparse.ArgumentParser()
     
     parser.add_argument("-o", "--outdir", help="Directory for contigs")
     parser.add_argument("-i")
+    parser.add_argument("--delim")
     
     return parser
 
 
 
-def generate():
-    pass
+def generate(outdir, input_table, delimiter):
+    
+    config_table = pd.read_csv(input_table, index_col=0, delimiter=delimiter)
+    
+    ...
+    
+    
+    
+    
+    
+    
