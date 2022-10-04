3dbdf16c6b0243cca14aab7f1c3959f5d570c0c0
Abusagit <mirotvorez00@gmail.com>
2022-10-04 15:06:53 +0000

    update: add defaults

--- graphmb_workflow/construct_graphmb_wf_config.py
+++ graphmb_workflow/construct_graphmb_wf_config.py
@@ -17,15 +17,16 @@ def get_parser():
     config_options.add_argument("-g", "--assembly_graph", help="location of assembly (contact) graph for GraphMB")
     config_options.add_argument("-d", "--depths_file", help="Contigs` depths file for VAMB")
     config_options.add_argument("-c", "--contact_map", help="location of contact map in .tsv format for embeddings aggregation")
-    config_options.add_argument("-p", "--cores", help="# of processors being used")
-    config_options.add_argument("--binner_output_root", help="Root for Binners (VAMB,  GraphMB) output dir" )
-    config_options.add_argument("--checkm_output_root", help="Root for CHECKM output dir")
-    config_options.add_argument("--aggregation_mode", help="Aggregation mode: full/during/after/none")
-    config_options.add_argument("-k", "--aggregation_neighbors", help="k nearest neighbors for aggregation")
-    config_options.add_argument("-i", "--aggregation_iterations", help="Iterations number for single iteration step")
-    config_options.add_argument("-m", "--contig_minlen", help="Threshold length for contig")
-    config_options.add_argument("-b", "--bin_minlen", help="Threshold length for bin in the output dir")
-    config_options.add_argument("--postfix_name")
+    
+    config_options.add_argument("-p", "--cores", help="# of processors being used", default="32")
+    config_options.add_argument("--binner_output_root", help="Root for Binners (VAMB,  GraphMB) output dir", default="outputs/")
+    config_options.add_argument("--checkm_output_root", help="Root for CHECKM output dir", default="checkm_reults/")
+    config_options.add_argument("--aggregation_mode", help="Aggregation mode: full/during/after/none", default="full")
+    config_options.add_argument("-k", "--aggregation_neighbors", help="k nearest neighbors for aggregation", default="30")
+    config_options.add_argument("-i", "--aggregation_iterations", help="Iterations number for single iteration step", default="1")
+    config_options.add_argument("-m", "--contig_minlen", help="Threshold length for contig", default="2000")
+    config_options.add_argument("-b", "--bin_minlen", help="Threshold length for bin in the output dir", default="500000")
+    config_options.add_argument("--postfix_name", default='')
         
     technical_args = {"output", "o"}
     return parser, technical_args
